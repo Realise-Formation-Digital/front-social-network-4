7f1a91f28afb260bffb2aeb3cbbb57331c98d68a
Dario <dariogallego2420@gmail.com>
2021-03-19 17:04:51 +0100

    created footer but problem with vertical alignment

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,19 @@
 <template>
+
     <div class="nav">
-            <p>Hola Que Tal ?</p>
+           
+        <router-link  to="/" >
+            <img id="logo-footer" src="../assets/logo/freelance-2.jpg" >
+        </router-link>
+
+        <div>
+            <p id="Icônes">icônes réseaux sociaux</p>
+
+            <p id="Copyrights">© 2021 BackPack, Inc. All rights reserved</p>
+        </div>
+
     </div>
+
 </template>
 
 
@@ -19,9 +31,38 @@
 
 <style>
     .nav{
-         background: #222;
-         color:white;
-         text-align: center;
+        background: #222;
+        color:white;
+        
+        position: fixed;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        
+         
     }
+
+    #logo-footer{
+        /*vertical-align: middle;*/
+        width: 7%;
+        margin-left: 4%;
+        margin-top: 2%;
+        
+        
+    }
+
+    #Copyrights{
+        text-align: center;
+        /*vertical-align: middle;*/
+        margin-bottom: 1%;
+    }
+
+    #Icônes{
+        text-align: right;
+        /*vertical-align: middle;*/
+        margin-right: 4%;
+    
+    }
+
    
 </style>
