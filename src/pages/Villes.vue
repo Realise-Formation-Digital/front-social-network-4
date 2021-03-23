ed79208ff7bd606c675c196d8a462ed030427801
Anton <grekov.anton96@gmail.com>
2021-03-23 17:22:32 +0100

    filtrage

--- src/pages/Villes.vue
+++ src/pages/Villes.vue
@@ -16,7 +16,7 @@
           <v-card
             outlined
             class="latestPostBlock"
-            @click="(continent = item.api), test(continent)"
+            @click="(continent = item.api), getTable();"
           >
             <v-img
               class="white--text align-end cardsImage"
@@ -50,7 +50,6 @@
               max="5"
               ticks="always"
               tick-size="4"
-              @change="test(filter.name + ' = ' + filter.value)"
             ></v-slider>
           </v-card-text>
         </v-col>
@@ -60,7 +59,7 @@
     <v-container align="center" justify="center">
       <v-btn depressed color="primary" block @click="show()">TEST</v-btn>
       <v-row>
-        <v-col v-for="item in table" :key="item.id" cols="12" md="4">
+        <v-col v-for="item in filteredTable" :key="item.id" cols="12" md="4">
           <v-card outlined class="mx-auto">
             <v-img
               class="white--text align-end"
@@ -92,84 +91,8 @@ export default {
 
   data() {
     return {
-      table: [],
+      
 
-      items: [
-        {
-          id: 1,
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-          description:
-            "His ubique laboramus ne. Expetenda assueverit sed ad. Id nec malis lucilius delicatissimi. Nec assum sonet suscipit ex, diam deterruisset ut usu, ad dicat fabellas aliquando eam.",
-          src: require("../assets/images/img10.jpg"),
-        },
-        {
-          id: 2,
-          title: "Christmas tales to read",
-          subtitle: "2,000 miles of wonder",
-          description:
-            "Sea ad habemus assueverit, omnes platonem convenire sit et, at integre pericula quo. Facete adolescens definitionem cu qui, in putant aliquid fierent ius.",
-          src: require("../assets/images/img11.jpg"),
-        },
-        {
-          id: 3,
-          title: "20 movies not to miss in 2020",
-          subtitle: "3,000 miles of wonder",
-          description:
-            "Aliquam albucius mei ei, debitis torquatos et pro, eos natum scribentur no. Putant verear constituto te qui. Adolescens persequeris vim ei. Vel nullam reprimique te.",
-          src: require("../assets/images/img12.jpg"),
-        },
-      ],
-
-      filters: [
-        { name: "Prix", value: 0 },
-        { name: "Internet", value: 0 },
-        { name: "Securité", value: 0 },
-        { name: "Distractions", value: 0 },
-        { name: "Appartements", value: 0 },
-      ],
-
-      language: null,
-      languages: [
-        { text: "french", name: "Français" },
-        { text: "german", name: "Allemand" },
-        { text: "english", name: "Anglais" },
-        { text: "spanish", name: "Espagnol" },
-        { text: "italian", name: "Italien" },
-      ],
-
-      continent: null,
-      continents: [
-        {
-          title: "Amerique",
-          api: "America",
-          src: require("../assets/images/America.jpg"),
-        },
-
-        {
-          title: "Europe",
-          api: "Europa",
-          src: require("../assets/images/Europe.jpeg"),
-        },
-
-        {
-          title: "Asia",
-          api: "Asia",
-          src: require("../assets/images/Asia.jpg"),
-        },
-
-        {
-          title: "Afrique",
-          api: "Africa",
-          src: require("../assets/images/Africa.jpg"),
-        },
-
-        {
-          title: "Oceanie",
-          api: "Oceania",
-          src: require("../assets/images/Australia.jpg"),
-        },
-      ],
     };
   },
 
